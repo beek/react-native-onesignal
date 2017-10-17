2a4a1e939a9785be7f7019820a093b3701e9795c
Max Holzheu <maxhm10@gmail.com>
2017-10-17 11:39:47 -0500

    try 5 seconds

--- ios/RCTOneSignal/RCTOneSignal.m
+++ ios/RCTOneSignal/RCTOneSignal.m
@@ -104,7 +104,7 @@ OSNotificationOpenedResult* coldStartOSNotificationOpenedResult;
     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:objectData
                                                          options:NSJSONReadingMutableContainers
                                                            error:&jsonError];
-    double delayInSeconds = 2.5;
+    double delayInSeconds = 5.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         [curRCTBridge.eventDispatcher sendAppEventWithName:@"remoteNotificationOpened" body:json];
